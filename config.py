ebe7d64213eff90f2cdce8cf27f51c363aae500e
Imad Bin Asad <105483337+imad10902@users.noreply.github.com>
2023-06-07 20:16:08 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ class Config(object):
     DEBUG = True
     TESTING = False
 
-class DevelopmentConfig(Config):
+class ProductionConfig(Config):
     SECRET_KEY = "this-is-a-super-secret-key"
     OPENAI_KEY = os.getenv("OPENAI_KEY")
 
