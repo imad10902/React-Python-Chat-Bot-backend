7a2dd322eb26269d498ce628d157f69df259f7f7
Imad Bin Asad <105483337+imad10902@users.noreply.github.com>
2023-06-07 20:18:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,6 @@ import aiapi
 app= Flask(__name__)
 app.config.from_object(config.config['production'])
 CORS(app)
-app.run(debug=True)
 @app.route("/", methods= ['POST', 'GET'])
 def home():
     if request.method == 'POST':
